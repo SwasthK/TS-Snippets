7b7ba5d434f44d5b873716abcfa6b96f40d2491b
SwasthK <220981@sdmcujire.in>
2024-02-28 23:52:39 +0530

    Change at ts.config && Interface,class integration

--- src/one.ts
+++ src/one.ts
@@ -11,7 +11,7 @@ function sum(a: number, b: string) {
     return a + b
 }
 
-console.log(sum(100, "kdkd"));
+console.log(sum(100, " pools"));
 
 function islegal(age: number): boolean {
     if (age > 18)
@@ -30,7 +30,6 @@ function lateprint(a: () => boolean) {
     }, 4000);
 }
 
-
 function dummy() {
     console.log("hello world");
 }
@@ -43,4 +42,56 @@ function runlate(b: () => void) {
 
 runlate(dummy)
 
+// ES2016 supported
 const myfun = (name: string) => `hello ${name}`
+
+
+function islegalcheck(user: {
+    name: string,
+    age: number
+}): boolean {
+    return (user.age > 18) ? true : false
+}
+
+console.log(islegalcheck({
+    name: "swasthik",
+    age: 9
+}))
+
+function myuser(You: you) {
+    return (You.age > 18) ? true : false
+}
+
+const You = {
+    age: 30,
+    name: "ew"
+}
+
+console.log(myuser(You));
+
+interface you {
+    age: number,
+    name?: string
+}
+
+interface Person {
+    name: string,
+    age: number
+    greet(msg: string): void
+}
+
+class Employee implements Person {
+    name: string;
+    age: number;
+    constructor(n: string, m: number) {
+        this.name = n;
+        this.age = m
+    }
+    greet(msg: string): void {
+        console.log("hello " + msg);
+    }
+}
+
+let e = new Employee("word2", 10)
+
+e.greet("word1");
